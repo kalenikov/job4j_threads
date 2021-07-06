12799a417fdf544f1b8f964fc2189d2ba14f7d79
serj <sergeykalenikov@gmail.com>
2021-07-06 11:14:51 +0300

    1.2. Режим ожидания. [#231217]

---
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -0,0 +1,17 @@
+package ru.job4j.concurrent;
+
+public class Wget {
+    public static void main(String[] args) {
+        new Thread(() -> {
+            int i = 0;
+            while (i < 100) {
+                System.out.print("\rLoading : " + ++i + "%");
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+}
