d40e5bac0e697c37f66a214b4e1a689c3ff0825a
serj <sergeykalenikov@gmail.com>
2021-07-16 16:42:32 +0300

    2. ExecutorService рассылка почты. [#63097]

---
+++ src/main/java/ru/job4j/pool/EmailNotificationPool.java
@@ -0,0 +1,30 @@
+package ru.job4j.pool;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class EmailNotificationPool {
+    private final ExecutorService pool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
+    public void emailTo(User user) {
+        pool.submit(() -> send(
+                String.format("Notification %s to email %s", user.getUsername(), user.getEmail()),
+                String.format("Add a new event to %s", user.getUsername()),
+                user.getEmail()));
+    }
+
+    private void send(String subject, String body, String email) {
+        System.out.println(Thread.currentThread().getName() + " send to " + email);
+    }
+
+    public void close() {
+        pool.shutdown();
+    }
+
+    public static void main(String[] args) {
+        EmailNotificationPool pool = new EmailNotificationPool();
+        pool.emailTo(new User("name1", "email1"));
+        pool.emailTo(new User("name2", "email2"));
+        pool.close();
+    }
+}
---
+++ src/main/java/ru/job4j/pool/User.java
@@ -0,0 +1,34 @@
+package ru.job4j.pool;
+
+import java.util.Objects;
+
+public class User {
+    private final String username;
+    private final String email;
+
+    public User(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(email, user.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(email);
+    }
+}
